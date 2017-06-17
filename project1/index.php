631b046b6aaabcf7016239e5e75f7c7ae5077564
thanhnn <nnt296@gmail.com>
2017-06-17 21:33:07 +0700

    move some files

---
+++ project1/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thanhnn
+ * Date: 6/17/2017
+ * Time: 12:19 PM
+ */
+header("Location: view/index.html");
---
+++ project1/model/logout.js
@@ -0,0 +1,17 @@
+var text = '<div class="alert alert-warning text-center">' +
+    '<strong>Warning!</strong> You have been logged out! Redirect in 3s!' +
+    '</div>';
+
+$(function () {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            $('#notification').html(text);
+            setTimeout(function () {
+                window.location.replace("../view/index.html");
+            }, 3000);
+        }
+    };
+    xmlhttp.open("GET", "../controller/logout.php?", false);
+    xmlhttp.send();
+})
