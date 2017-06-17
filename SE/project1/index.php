206c1886beefe418032d1dff600f51ab35c33f89
thanhnn <nnt296@gmail.com>
2017-06-17 21:10:02 +0700

    merge some files

---
+++ SE/project1/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thanhnn
+ * Date: 6/17/2017
+ * Time: 12:19 PM
+ */
---
+++ projectSE/project1/controller/user.php
@@ -0,0 +1,103 @@
+<?php
+ob_start();
+session_start();
+require_once('connectdb.php');
+mysqli_select_db($conn, 'user');
+if (isset($_POST['submit']) && isset($_SESSION['login_user'])) {
+
+    $date = date('Y-m-d H:i:s');
+    $email = $_SESSION['login_user'];
+    $data = json_decode($_POST['submit'], true);
+    $fname = $data[0]['fname'];
+    $lname = $data[0]['lname'];
+    $address = $data[0]['address'];
+    $dob = $data[0]['dob'];
+    $phone = $data[0]['phone'];
+
+    $password = $data[0]['password'];
+
+    $query = sprintf('UPDATE account
+                            SET First_Name="%s", Last_Name="%s", Address="%s", Birthday="%s", Password="%s", Phone="%s"
+                            WHERE Email="%s"', $fname, $lname, $address, $dob, $password, $phone, $email);
+
+    $result = mysqli_query($conn, $query);
+    if (!$result) {
+        die("Error database");
+    } else {
+        echo "Success";
+    }
+}
+if (isset($_GET['acc'])) {
+    if (!isset($_SESSION['login_user'])) {
+        die("Error");
+    }
+    $email = $_SESSION['login_user'];
+    if ($_GET['acc'] == 'profile') {
+        $query = sprintf("SELECT Email, First_Name,Last_Name,Phone,Address,date(Birthday) as Birthday FROM account WHERE Email='%s'", $email);
+        $result = mysqli_query($conn, $query);
+        $count = mysqli_num_rows($result);
+        if ($count != 1) {
+            die("Database problem!");
+        }
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $json = [];
+        array_push($json, $row);
+        $json = json_encode($json);
+        header('Content-Type: application/json');
+        echo $json;
+    }
+    if ($_GET['acc'] == 'edit') {
+        $query = sprintf("SELECT Password,First_Name,Last_Name,Phone,Address,date(Birthday) as Birthday FROM account WHERE Email='%s'", $email);
+        $result = mysqli_query($conn, $query);
+        $count = mysqli_num_rows($result);
+        $json = [];
+        if ($count < 1) {
+            $array = array(
+                "status" => "No record"
+            );
+            array_push($json, $array);
+            $json = json_encode($json);
+            header('Content-Type: application/json');
+            echo $json;
+            die();
+        }
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        array_push($json, $row);
+        $json = json_encode($json);
+        header('Content-Type: application/json');
+        echo $json;
+    }
+    if ($_GET['acc'] == 'history') {
+        $query = sprintf("SELECT UserID FROM account WHERE Email='%s'", $email);
+        $result = mysqli_query($conn, $query);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $userID = $row['UserID'];
+
+        $query = sprintf("SELECT f.Food_Name, t.Quantity, date(t.Date) as Date
+                                FROM transaction t NATURAL join dish d NATURAL JOIN food f
+                                WHERE t.UserID=%d
+                                ORDER BY t.Date", $userID);
+        $result = mysqli_query($conn, $query);
+        $count = mysqli_num_rows($result);
+        $json = [];
+        if ($count < 1) {
+            $array = array(
+                "status" => "No record"
+            );
+            array_push($json, $array);
+            $json = json_encode($json);
+            header('Content-Type: application/json');
+            echo $json;
+            die();
+        }
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            array_push($json, $row);
+        }
+        $json = json_encode($json);
+        header('Content-Type: application/json');
+        echo $json;
+    }
+}
+mysqli_close($conn);
+ob_end_flush();
+?>
